5b2df8ca426b908dce41d49710ee8c1bde1a86ea
phbailey <phbailey@0a76fd7e-f950-0410-93bd-addd55878803>
2010-09-09 23:20:11 +0000

    Working on groups & users
    git-svn-id: svn://svn.code.sf.net/p/ilabproject/code/trunk@279 0a76fd7e-f950-0410-93bd-addd55878803

--- iLabs/Services/iLabServiceBroker/admin/manageUsers.aspx.cs
+++ iLabs/Services/iLabServiceBroker/admin/manageUsers.aspx.cs
@@ -6,6 +6,7 @@
  */
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Configuration;
 using System.ComponentModel;
 using System.Data;
@@ -37,6 +38,7 @@ namespace iLabs.ServiceBroker.admin
 
 		protected void Page_Load(object sender, System.EventArgs e)
 		{
+            Object o = Events;
             defaultGroupID = wrapper.GetGroupIDWrapper(ServiceBroker.Administration.Group.NEWUSERGROUP);
 			if (Session["UserID"]==null)
 				Response.Redirect("../login.aspx");
@@ -115,19 +117,11 @@ namespace iLabs.ServiceBroker.admin
 		 */
 		private void BuildUserListBox()
 		{
-			lbxSelectUser.Items .Clear ();
-			
 			try
 			{
 				int[] userIDs = wrapper.ListUserIDsWrapper ();
-				User[] users = wrapper.GetUsersWrapper(userIDs);
-				foreach (User user in users) 
-				{
-					ListItem userItem = new ListItem();
-					userItem.Text = user.lastName+", "+user.firstName+" - "+user.userName;
-					userItem.Value = user.userID.ToString();
-					lbxSelectUser.Items .Add(userItem);
-				}
+				List<User> users = new List<User>(wrapper.GetUsersWrapper(userIDs));
+                BuildUserListBox(users);
 			}
 			catch(Exception ex)
 			{
@@ -141,31 +135,15 @@ namespace iLabs.ServiceBroker.admin
 		 * Builds the Select Users List using a specified array of users. 
 		 * This is used to return the results of a search
 		 */
-		private void BuildUserListBox(User[] users)
+		private void BuildUserListBox(List<User> users)
 		{
-			lbxSelectUser.Items .Clear ();
-			
+			lbxSelectUser.Items.Clear();
+            users.Sort();
 			foreach (User user in users) 
 			{
 				ListItem userItem = new ListItem();
-				userItem.Text = user.lastName+", "+user.firstName+" - "+user.userName;
-				userItem.Value = user.userID.ToString();
-				lbxSelectUser.Items .Add(userItem);
-			}
-		}
-
-		/* 
-		 * Builds the Select Users List using a specified list of users. 
-		 * This is used to return the results of a search
-		 */
-		private void BuildUserListBox(ArrayList users)
-		{
-			lbxSelectUser.Items .Clear ();
-			
-			foreach (User user in users) 
-			{
-				ListItem userItem = new ListItem();
-				userItem.Text = user.lastName+", "+user.firstName+" - "+user.userName;
+                userItem.Text = String.Format("{0,-20} --  {1}, {2}", user.userName, user.lastName, user.firstName);
+				//userItem.Text = user.lastName+", "+user.firstName+" - "+user.userName;
 				userItem.Value = user.userID.ToString();
 				lbxSelectUser.Items .Add(userItem);
 			}
@@ -191,7 +169,8 @@ namespace iLabs.ServiceBroker.admin
 			cbxLockAccount.Checked = false;
 			btnSaveChanges.CssClass="button";
 			btnSaveChanges.Enabled = true;
-
+            lblResponse.Text = "";
+            lblResponse.Visible = false; 
 		}
 
 		private void DisplayUserInfo(User user)
@@ -282,13 +261,6 @@ namespace iLabs.ServiceBroker.admin
 			}
 		}
 
-		private bool WildCardMatch(string searchString, string compareString )
-		{
-			Regex regex = new Regex(searchString,RegexOptions.IgnoreCase);
-
-			return regex.IsMatch(compareString);
-		}
-	
 		protected void lbxSelectUser_SelectedIndexChanged(object sender, System.EventArgs e)
 		{
 			if(lbxSelectUser.SelectedIndex < 0)
@@ -312,19 +284,22 @@ namespace iLabs.ServiceBroker.admin
 			}
 		}
 
-		protected void btnSearch_Click(object sender, System.EventArgs e)
-		{
-			//only superusers can view this page
-			if (!Session["GroupName"].ToString().Equals(ServiceBroker.Administration.Group.SUPERUSER))
-				btnSaveChanges.Enabled=false;
-				btnSaveChanges.CssClass="buttongray";
+        protected void btnSearch_Click(object sender, System.EventArgs e)
+        {
+            Object o = Events;
+            lblResponse.Text = "";
+            lblResponse.Visible = false;
+            //only superusers can view this page
+            if (!Session["GroupName"].ToString().Equals(ServiceBroker.Administration.Group.SUPERUSER))
+                btnSaveChanges.Enabled = false;
+            btnSaveChanges.CssClass = "buttongray";
 
-			// If an option is not selected in the "Search by" drop down list
-			if(ddlSearchBy.SelectedIndex<0)
-			{
-				lblResponse.Text = Utilities.FormatErrorMessage("Select a search criterion.");
-				lblResponse.Visible = true; 
-			}
+            // If an option is not selected in the "Search by" drop down list
+            if (ddlSearchBy.SelectedIndex < 0)
+            {
+                lblResponse.Text = Utilities.FormatErrorMessage("Select a search criterion.");
+                lblResponse.Visible = true;
+            }
             else if (ddlSearchBy.SelectedIndex == 0)
             {
                 BuildUserListBox();
@@ -339,44 +314,69 @@ namespace iLabs.ServiceBroker.admin
                 }
                 else
                 {
-                    ArrayList foundUsers = new ArrayList();
+                    List<User> foundUsers = new List<User>();
                     string option = ddlSearchBy.SelectedItem.Text;
 
-                    int[] userIDs = wrapper.ListUserIDsWrapper();
-                    User[] users = wrapper.GetUsersWrapper(userIDs);
+                    int[] userIDs = null;
+                    User[] users = null;
 
                     switch (option)
                     {
                         case "Username":
                             {
+                                userIDs = wrapper.ListUserIDsWrapper();
+                                users = wrapper.GetUsersWrapper(userIDs);
                                 foreach (User u in users)
                                 {
-                                    if (WildCardMatch(txtSearchBy.Text, u.userName))
+                                    if (Utilities.WildCardMatch(txtSearchBy.Text, u.userName))
                                         //if (! foundUsers.Contains(u))
                                         foundUsers.Add(u);
+                                }
+                                if (foundUsers.Count <= 0)
+                                {
+                                    string msg = "There are no usernames matching " + txtSearchBy.Text + ".";
+                                    lblResponse.Text = Utilities.FormatErrorMessage(msg);
+                                    lblResponse.Visible = true;
                                 }
                             }
                             break;
                         case "First Name":
                             {
+                                userIDs = wrapper.ListUserIDsWrapper();
+                                users = wrapper.GetUsersWrapper(userIDs);
                                 foreach (User u in users)
                                 {
-                                    if (WildCardMatch(txtSearchBy.Text, u.firstName))
+                                    if (Utilities.WildCardMatch(txtSearchBy.Text, u.firstName))
                                         foundUsers.Add(u);
+                                }
+                                if (foundUsers.Count <= 0)
+                                {
+                                    string msg = "There are no first names that match " + txtSearchBy.Text + ".";
+                                    lblResponse.Text = Utilities.FormatErrorMessage(msg);
+                                    lblResponse.Visible = true;
                                 }
                             }
                             break;
                         case "Last Name":
                             {
+                                userIDs = wrapper.ListUserIDsWrapper();
+                                users = wrapper.GetUsersWrapper(userIDs);
                                 foreach (User u in users)
                                 {
-                                    if (WildCardMatch(txtSearchBy.Text, u.lastName))
+                                    if (Utilities.WildCardMatch(txtSearchBy.Text, u.lastName))
                                         foundUsers.Add(u);
+                                }
+                                if (foundUsers.Count <= 0)
+                                {
+                                    string msg = "There are no last names that match " + txtSearchBy.Text + ".";
+                                    lblResponse.Text = Utilities.FormatErrorMessage(msg);
+                                    lblResponse.Visible = true;
                                 }
                             }
                             break;
                         case "Group":
                             {
+
                                 //Get a list of the groups from the database
                                 int[] groupIDs = wrapper.ListGroupIDsWrapper();
                                 ServiceBroker.Administration.Group[] groups = wrapper.GetGroupsWrapper(groupIDs);
@@ -385,12 +385,13 @@ namespace iLabs.ServiceBroker.admin
                                 ArrayList foundGroups = new ArrayList();
                                 foreach (ServiceBroker.Administration.Group g in groups)
                                 {
-                                    if (WildCardMatch(txtSearchBy.Text, g.groupName))
+                                    if (Utilities.WildCardMatch(txtSearchBy.Text, g.groupName))
                                         foundGroups.Add(g);
                                 }
 
                                 //if the group exists in the database
-                                ArrayList foundUserIDs = new ArrayList();
+                                List<int> foundUserIDs = new List<int>();
+
                                 if (foundGroups.Count > 0)
                                 {
                                     foreach (ServiceBroker.Administration.Group foundg in foundGroups)
@@ -400,67 +401,70 @@ namespace iLabs.ServiceBroker.admin
 
                                         //Put this in the foundUserID ArrayList
                                         foreach (int userID in userIDsForGroup)
+                                        {
                                             if (!foundUserIDs.Contains(userID))
                                                 foundUserIDs.Add(userID);
+                                        }
                                     }
 
                                     //if the group contains users
                                     if (foundUserIDs.Count > 0)
                                     {
-                                        User[] userArray = wrapper.GetUsersWrapper(Utilities.ArrayListToIntArray(foundUserIDs));
-                                        BuildUserListBox(userArray);
+                                        foundUsers.AddRange(wrapper.GetUsersWrapper(foundUserIDs.ToArray()));
+
                                     }
                                     else //no users exist in the group
                                     {
-                                        string msg = "No users exist in group " + txtSearchBy.Text + ".";
+                                        string msg = "No users exist in groups matching " + txtSearchBy.Text + ".";
                                         lblResponse.Text = Utilities.FormatErrorMessage(msg);
                                         lblResponse.Visible = true;
                                     }
                                 }
                                 else //groupID < 0, group doesn't exist
                                 {
-                                    string msg = "The group " + txtSearchBy.Text + " does not exist.";
+                                    string msg = "No group names matching " + txtSearchBy.Text + " exist.";
                                     lblResponse.Text = Utilities.FormatErrorMessage(msg);
                                     lblResponse.Visible = true;
                                 }
                             }
                             break;
+                        default:
+                            break;
                     } //end switch
 
-                    if (option.CompareTo("Group") != 0)
+                    if (foundUsers.Count > 0)
                     {
-                        if (foundUsers.Count > 0)
+                        // if only one record found
+                        if (foundUsers.Count == 1)
                         {
-                            // if only one record found
-                            if (foundUsers.Count == 1)
-                            {
-                                /* Need to rebuild the listbox, incase of multiple searches.
-                                 * The results of a search are displayed in the list box 
-                                 * & hence if one needs to do a 2nd search, the list has to be rebuilt.
-                                 */
-                                BuildUserListBox();
-                                User foundUser = (User)foundUsers[0];
-                                lbxSelectUser.Items.FindByValue(foundUser.userID.ToString()).Selected = true;
-                                DisplayUserInfo(foundUser);
-                            }
-                            else
-                            {
-                                BuildUserListBox(foundUsers);
-                            }
+                            /* Need to rebuild the listbox, incase of multiple searches.
+                             * The results of a search are displayed in the list box 
+                             * & hence if one needs to do a 2nd search, the list has to be rebuilt.
+                             */
+                            BuildUserListBox();
+                            User foundUser = foundUsers[0];
+                            lbxSelectUser.Items.FindByValue(foundUser.userID.ToString()).Selected = true;
+                            DisplayUserInfo(foundUser);
                         }
-                        else // no users found
+                        else
                         {
-                            string msg = "The user " + txtSearchBy.Text + " does not exist.";
-                            lblResponse.Text = Utilities.FormatErrorMessage(msg);
-                            lblResponse.Visible = true;
+                            BuildUserListBox(foundUsers);
+                            ResetState();
                         }
-                    }// end if option !group
+                    }
+                    else // no users found
+                    {
+                        lbxSelectUser.Items.Clear();
+                       // Error messages set in switch block
+                    }
                 }
             }
-		}
+        }
 	
 		protected void btnSaveChanges_Click(object sender, System.EventArgs e)
 		{
+            lblResponse.Text = "";
+            lblResponse.Visible = false; 
 			//Error checking for empty fields
 			if(txtUsername.Text.CompareTo("")==0 )
 			{
@@ -667,7 +671,11 @@ namespace iLabs.ServiceBroker.admin
 			txtSearchBy.Text = "";
 			BuildUserListBox();
 		}
-	}
+        protected void txtSearchBy_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+}
 	
 }
 
--- iLabs/Services/iLabServiceBroker/admin/manageusers.aspx
+++ iLabs/Services/iLabServiceBroker/admin/manageusers.aspx
@@ -1,36 +1,35 @@
 <%@ Register TagPrefix="uc1" TagName="footer" Src="../footer.ascx" %>
 <%@ Register TagPrefix="uc1" TagName="adminNav" Src="adminNav.ascx" %>
 <%@ Register TagPrefix="uc1" TagName="banner" Src="../banner.ascx" %>
-<%@ Page language="c#" Inherits="iLabs.ServiceBroker.admin.manageUser" CodeFile="manageUsers.aspx.cs" %>
+<%@ Page language="c#" Inherits="iLabs.ServiceBroker.admin.manageUser" CodeFile="manageUsers.aspx.cs"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<HTML>
-	<HEAD>
+<html>
+	<head>
 		<title>MIT iLab Service Broker - Manage Users</title> 
 		<!-- 
 Copyright (c) 2004 The Massachusetts Institute of Technology. All rights reserved.
 Please see license.txt in top level directory for full license. 
 -->
 		<!-- $Id: manageusers.aspx,v 1.1.1.1 2006/02/07 22:10:58 pbailey Exp $ -->
-		<meta content="Microsoft Visual Studio .NET 7.1" name="GENERATOR">
-		<meta content="C#" name="CODE_LANGUAGE">
-		<meta content="JavaScript" name="vs_defaultClientScript">
-		<meta content="http://schemas.microsoft.com/intellisense/ie5" name="vs_targetSchema">
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<meta content="Microsoft Visual Studio .NET 7.1" name="GENERATOR"/>
+		<meta content="C#" name="CODE_LANGUAGE"/>
+		<meta content="JavaScript" name="vs_defaultClientScript"/>
+		<meta content="http://schemas.microsoft.com/intellisense/ie5" name="vs_targetSchema"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
 		<style type="text/css">@import url( ../css/main.css ); 
 		</style>
-	</HEAD>
+</head>
 	<body>
 		<form id="Form1" method="post" runat="server">
 			<a name="top"></a>
-			<div id="outerwrapper"><uc1:banner id="Banner1" runat="server"></uc1:banner><uc1:adminnav id="AdminNav1" runat="server"></uc1:adminnav><br clear="all">
+			<div id="outerwrapper" ><uc1:banner id="Banner1" runat="server"></uc1:banner><uc1:adminnav id="AdminNav1" runat="server"></uc1:adminnav><br clear="all">
 				<div id="innerwrapper">
 					<div id="pageintro">
 						<h1>Manage Users</h1>
 						<p>Add or modify a User below.</p>
 						<!-- Errormessage should appear here:-->
-						<!--div class="errormessage"--><asp:label id="lblResponse" EnableViewState="False" Visible="False" Runat="server"></asp:label>
-						<!-- Manage Users Error message here: <div class="errormessage"><p>Error message here.</p></div> End error message --></div>
-					<!-- end pageintro div -->
+						<asp:label id="lblResponse" EnableViewState="False" Visible="False" Runat="server"></asp:label>
+					</div><!-- end pageintro div -->
 					<div id="pagecontent">
 						<div id="itemdisplay">
 							<h4>Selected User</h4>
@@ -81,18 +80,16 @@ Please see license.txt in top level directory for full license.
 										<th>
 											<label for="confirmpword">Confirm Password </label>
 										</th>
-										&nbsp;
 										<td><asp:textbox id="txtConfirmPassword" Runat="server" Width="152px" TextMode="Password"></asp:textbox></td>
 									</tr>
 									<tr>
 										<th>
 											&nbsp;</th>
-										<td><asp:checkbox id="cbxLockAccount" Runat="server"></asp:checkbox><label for="lock">Lock 
-												Account</label></td>
+										<td><asp:checkbox id="cbxLockAccount" Runat="server"></asp:checkbox><label for="lock">Lock Account</label></td>
 									</tr>
 									<tr>
-										<th colSpan="2">
-											<asp:button id="btnSaveChanges" Runat="server" Text="Save Changes" onclick="btnSaveChanges_Click"></asp:button><asp:button id="btnRemove" Runat="server" Text="Remove" CssClass="button" onclick="btnRemove_Click"></asp:button><asp:button id="btnNew" Runat="server" Text="New" CssClass="button" onclick="btnNew_Click"></asp:button></th></tr>
+										<th colspan="2">
+											<asp:button id="btnSaveChanges" Runat="server" OnClick="btnSaveChanges_Click" Text="Save Changes"></asp:button><asp:button id="btnRemove" Runat="server" OnClick="btnRemove_Click" Text="Remove" CssClass="button"></asp:button><asp:button id="btnNew" Runat="server" OnClick="btnNew_Click" Text="New" CssClass="button"></asp:button></th></tr>
 								</table>
 							</div>
 						</div>
@@ -104,17 +101,17 @@ Please see license.txt in top level directory for full license.
 								<asp:ListItem Value="Group">Group</asp:ListItem>
 							</asp:dropdownlist>
 							<br/><br/>
-							<asp:textbox id="txtSearchBy" Runat="server" Width="303px"></asp:textbox>&nbsp;&nbsp;<asp:button id="btnSearch" Runat="server" Text="Search" CssClass="button" onclick="btnSearch_Click"></asp:button>
+							<asp:textbox id="txtSearchBy" Runat="server" Width="303px" OnTextChanged="txtSearchBy_TextChanged"></asp:textbox>&nbsp;&nbsp;<asp:button id="btnSearch" Runat="server"  OnClick="btnSearch_Click" Text="Search" CssClass="button"></asp:button>
 						</div>
 						<div>&nbsp;</div>
-						<div class="simpleform"><label for="selectauser">Select a User (Last Name, First Name - 
-								Username)</label><br>
-							<asp:listbox cssClass="i18n" id="lbxSelectUser" Runat="server" Width="380px" AutoPostBack="True" Rows="15" onselectedindexchanged="lbxSelectUser_SelectedIndexChanged" Height="310px"></asp:listbox></div>
+						<div class="simpleform"><label for="selectauser">Select a User (Username --  Last Name, First Name)</label><br/>
+							<asp:listbox cssClass="i18n" id="lbxSelectUser" Runat="server" AutoPostBack="true"  OnSelectedIndexChanged="lbxSelectUser_SelectedIndexChanged" Width="380px" Rows="15"  Height="310px"></asp:listbox>
+						</div>
 					</div>
 					<br clear="all">
 					<!-- end pagecontent div --></div>
 				<!-- end innerwrapper div --><uc1:footer id="Footer1" runat="server"></uc1:footer></div>
 		</form>
-		<DIV></DIV>
+		<div></div>
 	</body>
-</HTML>
+</html>
