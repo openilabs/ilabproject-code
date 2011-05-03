311acd42e5c0b3ef3f795cb85dad27481e938e71
agabadave <agabadave@0a76fd7e-f950-0410-93bd-addd55878803>
2011-05-03 12:09:46 +0000

    The administrator website for configuring the SMS system before its put in use. Also contains a page for statical review before the user gets it in place.
    git-svn-id: svn://svn.code.sf.net/p/ilabproject/code/trunk@370 0a76fd7e-f950-0410-93bd-addd55878803

---
+++ PartnerLabs/MakerereU/SMS_Reservation/SMSrojectCode/WebSiteSMSConfiguration/Default.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+    .style12
+    {
+        font-size: x-large;
+        font-weight: bold;
+    }
+    
+        .style13
+        {
+            font-size: large;
+            font-weight: bold;
+        }
+        
+        .style14
+        {
+        	background: #F6FFFF;
+        	
+        }
+        
+        .styleButton
+        {
+         background-color: #006699;
+         font-weight:bold;
+         color:Black;
+         border-color: #0099CC;
+         height: 28px;
+         width: 175px;
+         azimuth:left-side;
+        }
+        
+       
+
+    
+    </style>
+</asp:Content>
+
+<asp:Content ID="Content2" runat="server" 
+    contentplaceholderid="ContentPlaceHolder1">
+    <p class="style13">
+        &nbsp;<h2>Instatnt Message Configuration</h2></p>
+<p>
+    This Page displays the available configurations that the Instant Message 
+    application is holding.</p>
+    <p>
+        &nbsp;</p>
+<p style="text-align: right">
+    
+    <asp:Button ID="NewLabConfiguration" CssClass="styleButton" runat="server" 
+        onclick="NewLabConfiguration_Click" Text="New Configuration"  />
+    &nbsp;</p>
+    <div class ="style14">
+    <h3>
+    Lab Name:<asp:Label ID="LabelClientName" runat="server" ></asp:Label>
+    </h3>
+        <h4>Description:</h4>
+        <p> 
+            <asp:Label ID="LbClientDescription" runat="server" ></asp:Label>
+        </p>
+        <p style="text-align: right"> 
+            
+    </div>
+
+</asp:Content>
+
+
---
+++ PartnerLabs/MakerereU/SMS_Reservation/SMSrojectCode/WebSiteSMSConfiguration/PopupLabSetup.aspx.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class PopupLabSetup : System.Web.UI.Page
+{
+    protected int LabConfID;        //probably will not need to refer
+    protected string LabNm = null;
+    protected string LabDescpt = null;
+    protected string ExptGrpN = null;
+    protected string sbGuid = null;
+    protected string InPasKeyNm = null;
+    protected int InPasKyID;
+    protected int OutPasKyID;
+    protected string OutPasKyNm = null;
+    protected string sbURL = null;
+    protected string ClientNm = null;
+    protected string ClientGUID = null;
+    protected string LsName = null;
+    protected string LsGuid = null;
+    protected string IMGuid = null;
+    protected int MinDuratn;
+    protected int MaxDuratn;
+    
+
+    
+    //string Lab_Name = LabNameInput.Text;
+    //string Lab_Description = LabDescriptionInput.Text;
+    //string Group_Name = ExperimentGroupInPut.Text;
+    
+    protected void CreateBtn_Click(object sender, EventArgs e)
+    {
+        if (TxBLabName.Text == "" && TxBLabDescription.Text == "" && TxBExptGroup.Text == "" &&
+             TxBSbGuid.Text == "" && TxBInPasskeyName.Text == "" && TxBInPasskey.Text == "" &&
+             TxBOutPasskey.Text == "" && TxBOutPasskeyName.Text == "" && TxBSbUrl.Text == "" &&
+             TxBClientName.Text == "" && TxBClientGuid.Text == "" && TxBminLabDurn.Text == "" &&
+             TxBmaxLabDurn.Text == "" && TxBMessageGuid.Text == "")
+        {
+            Confirmation.Text = "No value has been inserted";
+        }
+
+        else if (TxBLabName.Text == "" || TxBLabDescription.Text == "" || TxBExptGroup.Text == "" ||
+             TxBSbGuid.Text == "" || TxBInPasskeyName.Text == "" || TxBInPasskey.Text == "" ||
+             TxBOutPasskey.Text == "" || TxBOutPasskeyName.Text == "" || TxBSbUrl.Text == "" ||
+             TxBClientName.Text == "" || TxBClientGuid.Text == "" || TxBminLabDurn.Text == "" ||
+             TxBmaxLabDurn.Text == "" || TxBMessageGuid.Text == "")
+        {
+            Confirmation.Text = "some missing values";
+        }
+
+        else
+        {
+            if (IsInteger(TxBInPasskey.Text) == false || IsInteger(TxBOutPasskey.Text) == false ||
+                IsInteger(TxBminLabDurn.Text) == false || IsInteger(TxBmaxLabDurn.Text) == false) 
+            {
+                Confirmation.Text = "Please check if the following are integers:";
+                Confirmation.Text += "<br>In Passkey ID";
+                Confirmation.Text += "<br>Out passkey ID";
+                Confirmation.Text += "<br>Minimum Lab Duration";
+                Confirmation.Text += "<br>Maximum Lab duration";
+            }
+            
+            else
+            {
+        
+        LabNm = TxBLabName.Text;
+        LabDescpt = TxBLabDescription.Text;
+        ExptGrpN = TxBExptGroup.Text;
+        sbGuid = TxBSbGuid.Text;
+        InPasKeyNm = TxBInPasskeyName.Text;
+        InPasKyID = Convert.ToInt32(TxBInPasskey.Text);
+        OutPasKyID = Convert.ToInt32(TxBOutPasskey.Text);
+        OutPasKyNm = TxBOutPasskeyName.Text;
+        sbURL = TxBSbUrl.Text;
+        ClientNm = TxBClientName.Text;
+        ClientGUID = TxBClientGuid.Text;
+        LsName = TxBLsName.Text;
+        LsGuid = TxBLsGuid.Text;
+        MinDuratn = Convert.ToInt32(TxBminLabDurn.Text);
+        MaxDuratn = Convert.ToInt32(TxBmaxLabDurn.Text);
+        IMGuid = TxBMessageGuid.Text;
+
+        //insert into database
+        Confirmation.Text = AddToDB();
+        //add some error checks
+            }
+
+       }
+
+    }
+
+    
+    protected void ClosePopupButton_Click(object sender, EventArgs e)
+    {
+        // string clientScript="window.close(); return false;";
+
+
+    }
+
+    
+   
+
+    protected string AddToDB()
+    {
+        try
+        {
+            DataClassesDataContext db = new DataClassesDataContext();
+            //LabConfiguration v = dx.LabConfigurations.Where(s => s.LabConfigurationID > 34).First();
+            //v.LabDescription = "wefwedf";
+            //dx.SubmitChanges();
+
+            LabConfiguration lc = new LabConfiguration();
+            lc.LabName = TxBLabName.Text;
+            lc.LabDescription = LabDescpt;
+            lc.ExperimentGroupName = ExptGrpN;
+            lc.ServiceBrokerGUID = sbGuid;
+            lc.InstantMessageInPasskey = InPasKeyNm;
+            lc.InstantMessageOutPassKey = OutPasKyNm;
+            lc.InstantMessageInID = InPasKyID;
+            lc.InstantMessageOutID = OutPasKyID;
+            lc.ServiceBrokerURL = sbURL;
+            lc.ClientName = ClientNm;
+            lc.ClientGuid = ClientGUID;
+            lc.InstantMessageGUID = IMGuid;
+            lc.MaximumLabDuration = MaxDuratn;
+            lc.MinimumLabDuration = MinDuratn;
+            lc.LabServerName = LsName;
+            lc.LabServerGuid = LsGuid;
+
+            db.LabConfigurations.InsertOnSubmit(lc);
+            db.SubmitChanges();
+
+            return "Instant Scheduling configuration for Client " + ClientName.Text + "has been succesfully saved.";
+        }
+        catch
+        {
+            return "Failed to connect to database";
+        }
+    }
+
+    
+   protected bool IsInteger(string theValue)
+    {
+        try
+        {
+            Convert.ToInt32(theValue);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
