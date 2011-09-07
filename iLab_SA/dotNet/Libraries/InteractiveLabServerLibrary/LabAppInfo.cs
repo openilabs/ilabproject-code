2527364f2f99ed3ee1be9527b938d9a3b090789a
phbailey <phbailey@0a76fd7e-f950-0410-93bd-addd55878803>
2011-09-07 19:44:37 +0000

    added header info
    git-svn-id: svn://svn.code.sf.net/p/ilabproject/code/trunk@444 0a76fd7e-f950-0410-93bd-addd55878803

--- iLab_SA/dotNet/Libraries/InteractiveLabServerLibrary/LabAppInfo.cs
+++ iLab_SA/dotNet/Libraries/InteractiveLabServerLibrary/LabAppInfo.cs
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2004 The Massachusetts Institute of Technology. All rights reserved.
+ * Please see license.txt in top level directory for full license.
+ */
+
+/* $Id:$ */
+
 using System;
 
 namespace iLabs.LabServer.Interactive
