045fb594c336e90063cfd4bfaa080a6cb65805c5
phbailey <phbailey@0a76fd7e-f950-0410-93bd-addd55878803>
2012-01-17 22:21:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/ilabproject/code/trunk@495 0a76fd7e-f950-0410-93bd-addd55878803

--- iLab_SA/Database/sqlServer/DB_Scripts/ProcessAgent/ProcessAgentProcedures.sql
+++ iLab_SA/Database/sqlServer/DB_Scripts/ProcessAgent/ProcessAgentProcedures.sql
@@ -1727,37 +1727,74 @@ AS
 BEGIN
 Create Table #ticks  (tId bigint,cId bigint, isGuid varchar (50))
 create Table #coups (cId bigint,  isGuid varchar (50))
-create Table #remove (cnt int, cid bigint, isGuid varchar (50))
+create table #coupAfter (cid bigint, isGuid varchar (50))
+Create table #domains (isGuid varchar (50))
+create Table #remove (cid bigint)
+
 Declare @numTicks int
+SET @numTicks = 0
 Declare @numCoupons int
-insert #ticks Exec('select ticket_id, Coupon_ID, issuer_guid from ticket
+SET @numCoupons = 0;
+Declare @numCoup int
+SET @numCoup = 0;
+Declare @domain varchar(50)
+
+insert into #ticks select ticket_id, Coupon_ID, issuer_guid from ticket
 	where cancelled = 1 OR 
-	(duration != -1 and (DATEDIFF(second,creation_Time,GETUTCDATE()) > duration))')
+	(duration != -1 and (DATEDIFF(second,creation_Time,GETUTCDATE()) > duration))
+SET @numTicks = @@ROWCOUNT;
 	
-insert #coups Exec('select distinct cid,isGuid from #ticks')
-BEGIN
+insert into #coups select distinct cid,isGuid from #ticks
+
 BEGIN TRANSACTION
 delete from Ticket where Ticket_ID IN (select tId from #ticks)
-if (@@error > 0)
+if (@@error <> 0)
+		BEGIN
+			ROLLBACK
 			goto on_error
-SET @numTicks = @@ROWCOUNT;
+		END
 
+insert into #coupAfter select distinct coupon_ID, issuer_guid from Ticket
+where Coupon_ID in (Select cid from #coups)  group by coupon_ID,issuer_guid
 
-insert #remove Exec( 'select COUNT(coupon_ID), coupon_ID, issuer_guid from Ticket
-where Coupon_ID in (Select cid from #coups)  group by coupon_ID,issuer_guid')
+insert into #domains select distinct isGuid from #coupAfter
 
-delete from Coupon where Coupon_ID in ( select cid from #remove where cnt = 0 )
-if (@@error > 0)
+DECLARE domainC CURSOR  LOCAL
+for select isGuid from #domains
+OPEN domainC
+
+FETCH domainC INTO @domain
+	While (@@FETCH_STATUS <> -1)
+	BEGIN 
+		TRUNCATE TABLE #remove
+		insert into #remove select cid from #coups where isGuid = @domain
+		delete from #remove where cid in (select cid from #coupAfter where isGuid = @domain)
+		set @numCoup = (select COUNT(*) from #remove)
+		set @numCoupons = @numCoupons + @numCoup
+		delete from Coupon where issuer_guid = @domain AND Coupon_ID in ( select cid from #remove)
+		if (@@error <> 0)
+		BEGIN
+			ROLLBACK
+			CLOSE domainC
+			DEALLOCATE domainC
 			goto on_error
-SET @numCoupons = @@ROWCOUNT
+		END
+		FETCH domainC INTO @domain
+	END
+
+CLOSE domainC
+DEALLOCATE domainC
+
 COMMIT TRANSACTION
-END
+
 Drop Table #ticks
 Drop Table #coups
 Drop Table #remove
 select @numTicks, @numCoupons
+return 0
 on_error: 
 	ROLLBACK TRANSACTION
+	return -1
 END
 GO
 
