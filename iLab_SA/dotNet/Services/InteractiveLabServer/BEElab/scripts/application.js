67061798b1fa35193e52a800a8bd7b71710b4838
phbailey <phbailey@0a76fd7e-f950-0410-93bd-addd55878803>
2012-05-23 20:21:03 +0000

    messy but working
    git-svn-id: svn://svn.code.sf.net/p/ilabproject/code/trunk@559 0a76fd7e-f950-0410-93bd-addd55878803

--- iLab_SA/dotNet/Services/InteractiveLabServer/BEElab/scripts/application.js
+++ iLab_SA/dotNet/Services/InteractiveLabServer/BEElab/scripts/application.js
@@ -14,6 +14,69 @@
       return this.chamber;
     };
 
+    Application.prototype.selectDiv = function(holder) {
+      var range;
+      this.clearSelection();
+      if (document.selection) {
+        range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(holder));
+        return range.select();
+      } else if (window.getSelection) {
+        range = document.createRange();
+        range.selectNode(document.getElementById(holder));
+        return window.getSelection().addRange(range);
+      }
+    };
+
+    Application.prototype.clearSelection = function() {
+      if (document.selection) {
+        return document.selection.empty();
+      } else if (window.getSelection) {
+        return window.getSelection().removeAllRanges();
+      }
+    };
+
+    Application.prototype.exportData = function() {
+      var baseSerie, chart, extremes, indexes;
+      chart = this.chamber.chart;
+      baseSerie = chart.xAxis[0];
+      extremes = baseSerie.getExtremes();
+      indexes = this.getIndexes(extremes);
+      ($('#data-holder')).text(this.extractData(indexes[0], indexes[1]).toString());
+      return ($('.hidden')).show();
+    };
+
+    Application.prototype.extractData = function(from, to) {
+      var i, _i, _results;
+      _results = [];
+      for (i = _i = from; from <= to ? _i <= to : _i >= to; i = from <= to ? ++_i : --_i) {
+        _results.push(window.sampleData[i]);
+      }
+      return _results;
+    };
+
+    Application.prototype.getIndexes = function(extremes) {
+      return [this.binarySearch(extremes.min), this.binarySearch(extremes.max)];
+    };
+
+    Application.prototype.binarySearch = function(timestamp) {
+      var high, low, mid, time;
+      low = 0;
+      high = window.sampleData.length - 1;
+      while (high >= low) {
+        mid = Math.floor((low + high) / 2);
+        time = Date.parse(window.sampleData[mid][1]);
+        if (time > timestamp) {
+          high = mid - 1;
+        } else if (time < timestamp) {
+          low = mid + 1;
+        } else {
+          return mid;
+        }
+      }
+      return low - 1;
+    };
+
     return Application;
 
   })();
@@ -30,7 +93,7 @@
 
       this.chart = 0;
       this.maxPoints = 60;
-      this.series = 0;
+      this.series = [];
       this.initializeChart(window.sampleData, new Date());
       this.memoSeries();
     }
@@ -39,10 +102,8 @@
       var $collection;
       $collection = $(".series-box");
       return _.map($collection, function(checkbox) {
-        var jBox;
-        jBox = $(checkbox);
         return _.map(rawData, function(data) {
-          return [Date.parse(data[0]), data[jBox.data('array-index')]];
+          return [Date.parse(data[1]), data[checkbox.value]];
         });
       });
     };
@@ -80,14 +141,35 @@
           defaultSeriesType: 'line',
           zoomType: 'x'
         },
+        exporting: {
+          filename: "BEE Chart",
+          width: 1300,
+          buttons: {
+            dataExportButton: {
+              hoverSymbolFill: '#768F3E',
+              onclick: function() {
+                return window.app.exportData();
+              },
+              symbol: 'exportIcon',
+              align: 'right',
+              x: -62
+            }
+          }
+        },
         rangeSelector: {
           buttons: [
             {
               type: 'all',
               text: 'All'
+            }, {
+              type: 'day',
+              count: 1,
+              text: '1d'
             }
-          ]
+          ],
+          selected: 1
         },
+        navigator: {},
         title: {
           text: 'Test Chamber Temperature',
           x: -20
@@ -129,17 +211,18 @@
 
   jQuery(function($) {
     var application;
-    application = new Application;
-    return ($('.series-box')).change(function(event) {
+    window.app = application = new Application;
+    ($('.series-box')).live('change', function(event) {
       var index, serie;
       index = application.chamber.series[this.name];
       serie = application.chamber.chart.series[index];
-      if (serie.visible) {
-        return serie.hide();
-      } else {
+      if ($(this).attr("checked") === "checked") {
         return serie.show();
+      } else {
+        return serie.hide();
       }
     });
+    return ($('.series-box')).trigger('change');
   });
 
 }).call(this);
