f7aabba9d70597c87b8e6ace032be4a405fc776c
sanabria <sanabria@0a76fd7e-f950-0410-93bd-addd55878803>
2013-11-04 18:43:58 +0000

    Fixed issue with slider and added selector for number of hours
    git-svn-id: svn://svn.code.sf.net/p/ilabproject/code/trunk@781 0a76fd7e-f950-0410-93bd-addd55878803

--- PartnerLabs/MIT/BEElab/Harry's test sun lamp graph/chart.js
+++ PartnerLabs/MIT/BEElab/Harry's test sun lamp graph/chart.js
@@ -9,108 +9,108 @@
  for (int i = 0; i < 24-sunHours; i++) {
     sunData.push(0);
  }
-//  var colors = Highcharts.getOptions().colors;
-//     chart = new Highcharts.Chart({
-//     chart: {
-//         renderTo: 'container',
-//         type: 'column'
+ var colors = Highcharts.getOptions().colors;
+    chart = new Highcharts.Chart({
+    chart: {
+        renderTo: 'container',
+        type: 'column'
 
-//     },
+    },
     
-//     xAxis: {
-//         min:0,
-//         max:23,
-//         id:"my xaxis",
-//         tickInterval: 1,
-//         title: {
-//             enabled: true,
-//             margin: 15
-//         },
-//         title: {
-//             text: 'time'
-//         },
-//         labels: {
-//             enabled: false
-//         }
-//     },
-//     scrollbar: {
-//         enabled: true
-//     },
-//     yAxis: {
-//         gridLineWidth: 0,
-//         minorGridLineWidth: 0,
-//         lineColor: 'transparent',        
-//         labels: {
-//             enabled: false
-//         },
-//         minorTickLength: 0,
-//         tickLength: 0,
-//         title: {
-//             text: 'Sun Lamp'
-//         },
-//         max: 1,
-//         min: 0,
-//         stackLabels: {
-//             enabled: true,
-//             style: {
-//                 fontWeight: 'bold',
-//                 color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-//             }
-//         }
-//     },
-//     legend: {
-//         align: 'center',
-//         //x: -100,
-//         verticalAlign: 'top',
-//         //y: 20,
-//         floating: true,
-//         backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColorSolid) || 'white',
-//         borderColor: '#CCC',
-//         borderWidth: 1,
-//         shadow: false
-//     },
-//     tooltip: {
-//         formatter: function () {
-//             return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y + '<br/>'; //+
-//             //'Total: '+ this.point.stackTotal;
-//         }
-//     },
-//     plotOptions: {
-//         column: {
-//             stacking: 'normal',
-//             point: {
-//                 events: {
-//                     click: function () {
-//                         console.log('this.x,y=' + this.x + "," + this.y + " category=" + this.category);
+    xAxis: {
+        min:0,
+        max:23,
+        id:"my xaxis",
+        tickInterval: 1,
+        title: {
+            enabled: true,
+            margin: 15
+        },
+        title: {
+            text: 'time'
+        },
+        labels: {
+            enabled: false
+        }
+    },
+    scrollbar: {
+        enabled: true
+    },
+    yAxis: {
+        gridLineWidth: 0,
+        minorGridLineWidth: 0,
+        lineColor: 'transparent',        
+        labels: {
+            enabled: false
+        },
+        minorTickLength: 0,
+        tickLength: 0,
+        title: {
+            text: 'Sun Lamp'
+        },
+        max: 1,
+        min: 0,
+        stackLabels: {
+            enabled: true,
+            style: {
+                fontWeight: 'bold',
+                color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+            }
+        }
+    },
+    legend: {
+        align: 'center',
+        //x: -100,
+        verticalAlign: 'top',
+        //y: 20,
+        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColorSolid) || 'white',
+        borderColor: '#CCC',
+        borderWidth: 1,
+        shadow: false
+    },
+    tooltip: {
+        formatter: function () {
+            return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y + '<br/>'; //+
+            //'Total: '+ this.point.stackTotal;
+        }
+    },
+    plotOptions: {
+        column: {
+            stacking: 'normal',
+            point: {
+                events: {
+                    click: function () {
+                        console.log('this.x,y=' + this.x + "," + this.y + " category=" + this.category);
 
-//                     }
-//                 }
-//             },
-//             dataLabels: {
-//                 enabled: true,
-//                 color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-//             }
-//         }
-//     },
+                    }
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+            }
+        }
+    },
     
-//     plotOptions: {
-//         series: {
-//             marker: {
-//                 enabled: false,
-//                 id: "my scrollbar"
-//             }
-//         },
-//         column: {
-//             pointPadding: 0,
-//             pointRange : 2.5,
-//             borderWidth: 0,
-//             pointPlacement: 'between'
-//         }
-//     },
+    plotOptions: {
+        series: {
+            marker: {
+                enabled: false,
+                id: "my scrollbar"
+            }
+        },
+        column: {
+            pointPadding: 0,
+            pointRange : 2.5,
+            borderWidth: 0,
+            pointPlacement: 'between'
+        }
+    },
     
-//     series: [{
-//         data: sunData
-//         }]
+    series: [{
+        data: sunData
+        }]
     
     
-// });
+});
