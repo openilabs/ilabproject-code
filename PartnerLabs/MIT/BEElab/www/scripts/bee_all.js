e8c4380e02dc4ac67bdb1a82e533ca59198e6141
sanabria <sanabria@0a76fd7e-f950-0410-93bd-addd55878803>
2013-11-25 21:07:45 +0000

    sunchart *should* be at the top of the existing chart.  no guarantees.
    git-svn-id: svn://svn.code.sf.net/p/ilabproject/code/trunk@824 0a76fd7e-f950-0410-93bd-addd55878803

--- PartnerLabs/MIT/BEElab/www/scripts/bee_all.js
+++ PartnerLabs/MIT/BEElab/www/scripts/bee_all.js
@@ -83,14 +83,14 @@
     };
 
     Lab.prototype._loadMap = {
-    	'Load 1': 0,
-      'Load 2': 1,
-      'Load 3': 2,
-      'Load 4': 3,
-      0: '100',
-      1: '100',
+    	'Load 1': 2,
+      'Load 2': 3,
+      'Load 3': 4,
+      'Load 4': 5,
       2: '100',
-      3: '100'
+      3: '100',
+      4: '100',
+      5: '100'
     };
     console.log("Lab.prototype._loadMap is called");
     return Lab;
@@ -110,19 +110,19 @@
    
     
      //initialize the slider.  may not want 1500px size, need to test and see.
-//    sunSlider = new dhtmlxSlider("divSunSlider",180, "arrow", false, 0, 24-sunHours, 0, 1);
-//    sunSlider.setImagePath("scripts/dhtmlxSlider/codebase/imgs/");
-//    sunSlider.attachEvent("onChange", shiftSunGraph);
-//    sunSlider.init();
-// 
-//    
-//     sunData = new Array();
-//      for (var i = 0; i < sunHours; i++) {
-//         sunData.push(1);
-//     }
-//     for (var i = 0; i < 24-sunHours; i++) {
-//        sunData.push(0);
-//    }
+   sunSlider = new dhtmlxSlider("divSunSlider",180, "arrow", false, 0, 24-sunHours, 0, 1);
+   sunSlider.setImagePath("scripts/dhtmlxSlider/codebase/imgs/");
+   sunSlider.attachEvent("onChange", shiftSunGraph);
+   sunSlider.init();
+
+   
+    sunData = new Array();
+     for (var i = 0; i < sunHours; i++) {
+        sunData.push(1);
+    }
+    for (var i = 0; i < 24-sunHours; i++) {
+       sunData.push(0);
+   }
     
       this.lab = new Lab(window.labLength);
       $("#chart-container").css("width", window.labLength * 42);
@@ -268,7 +268,7 @@ function drawGraphWithProfile(profileSelected) {
 
 function redrawChart() {
   
-  if (chart1.series.length < 5) {    
+  if (chart1.series.length < 7) {    
     chart1.addSeries({
         data: SeriesToBeDisplayed,
         name: 'Temperature',
@@ -278,35 +278,35 @@ function redrawChart() {
    });
    }
    else{
-        chart1.series[4].setData(SeriesToBeDisplayed);
+        chart1.series[6].setData(SeriesToBeDisplayed);
     }
 }
 
-//var shiftSunGraph = function (sliderValue, sliderObj) {
-//    newData = new Array();
-//    for (var i = 0; i < sliderValue; i++) {
-//        newData.push(0);
-//    }
-//    for (var i = 0; i < sunHours; i++) {
-//        newData.push(1);
-//    }
-//    for (var i = 0; i < 24-sunHours-sliderValue; i++) {
-//        newData.push(0);
-//    }
-//    sunData = newData;
-//    //this updates the chart with the new start position of the sun
-//    //chart.series[0].update({data: sunData}, true);
-//    sunChart.series[0].setData(newData);
-//    return null;
-// }
+var shiftSunGraph = function (sliderValue, sliderObj) {
+   newData = new Array();
+   for (var i = 0; i < sliderValue; i++) {
+       newData.push(0);
+   }
+   for (var i = 0; i < sunHours; i++) {
+       newData.push(1);
+   }
+   for (var i = 0; i < 24-sunHours-sliderValue; i++) {
+       newData.push(0);
+   }
+   sunData = newData;
+   //this updates the chart with the new start position of the sun
+   //chart.series[0].update({data: sunData}, true);
+   sunChart.series[0].setData(newData);
+   return null;
+}
 
-//$(function() {
-//    $("#sun_hours_select").change(function () {
-//        sunHours = parseInt($("#sun_hours_select").val());
-//        shiftSunGraph(0, null);
-//        sunSlider.setMax(24-sunHours);
-//    })
-//});
+$(function() {
+   $("#sun_hours_select").change(function () {
+       sunHours = parseInt($("#sun_hours_select").val());
+       shiftSunGraph(0, null);
+       sunSlider.setMax(24-sunHours);
+   })
+});
 
 
 // //initialize the slider.  may not want 1500px size, need to test and see.
@@ -318,119 +318,119 @@ function redrawChart() {
 
 
  // create initial data with lamp starting at hour 0.
-// var colors = Highcharts.getOptions().colors;
+var colors = Highcharts.getOptions().colors;
 
-// sunChart = new Highcharts.Chart({
-//    chart: {
-//        renderTo: 'sun-container',
-//        type: 'column'
+sunChart = new Highcharts.Chart({
+   chart: {
+       renderTo: 'sun-container',
+       type: 'column'
 
-//    },
-//    
-//    xAxis: {
-//        min:0,
-//        max:23,
-//        id:"my xaxis",
-//        tickInterval: 1,
-//        title: {
-//            enabled: true,
-//            margin: 15
-//        },
-//        title: {
-//            text: 'time'
-//        },
-//        labels: {
-//            enabled: true,
-//            x: -35
-//        },
-//        tickmarkPlacement: 'on',
-//        showFirstLabel: false,
-//        tickLength: 0
-//    },
-//    
-//    yAxis: {
-//        gridLineWidth: 0,
-//        minorGridLineWidth: 0,
-//        lineColor: 'transparent',        
-//        labels: {
-//            formatter: function() {
-//                if (this.value == 1) {
-//                  return "On";
-//                }
-//                else if (this.value == 0) {
-//                    return "Off";
-//                }
-//                return "";
-//            },
-//            step: 1
-//        },
-//        minorTickLength: 0,
-//        tickLength: 0,
-//        title: {
-//            text: 'Sun Lamp'
-//        },
-//        max: 1,
-//        min: 0,
-//        stackLabels: {
-//            enabled: true,
-//            style: {
-//                fontWeight: 'bold',
-//                color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-//            }
-//        }
-//    },
-//    legend: {
-//        enabled: false
-//    },
-//    title: {
-//            text: "Sun Lamp"
-//    },
-//    tooltip: {
-//        formatter: function () {
-//            return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y + '<br/>'; //+
-//            //'Total: '+ this.point.stackTotal;
-//        }
-//    },
-//    plotOptions: {
-//        column: {
-//            stacking: 'normal',
-//            point: {
-//                events: {
-//                    click: function () {
-//                        console.log('this.x,y=' + this.x + "," + this.y + " category=" + this.category);
+   },
+   
+   xAxis: {
+       min:0,
+       max:23,
+       id:"my xaxis",
+       tickInterval: 1,
+       title: {
+           enabled: true,
+           margin: 15
+       },
+       title: {
+           text: 'time'
+       },
+       labels: {
+           enabled: true,
+           x: -35
+       },
+       tickmarkPlacement: 'on',
+       showFirstLabel: false,
+       tickLength: 0
+   },
+   
+   yAxis: {
+       gridLineWidth: 0,
+       minorGridLineWidth: 0,
+       lineColor: 'transparent',        
+       labels: {
+           formatter: function() {
+               if (this.value == 1) {
+                 return "On";
+               }
+               else if (this.value == 0) {
+                   return "Off";
+               }
+               return "";
+           },
+           step: 1
+       },
+       minorTickLength: 0,
+       tickLength: 0,
+       title: {
+           text: 'Sun Lamp'
+       },
+       max: 1,
+       min: 0,
+       stackLabels: {
+           enabled: true,
+           style: {
+               fontWeight: 'bold',
+               color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+           }
+       }
+   },
+   legend: {
+       enabled: false
+   },
+   title: {
+           text: "Sun Lamp"
+   },
+   tooltip: {
+       formatter: function () {
+           return '<b>' + this.x + '</b><br/>' + this.series.name + ': ' + this.y + '<br/>'; //+
+           //'Total: '+ this.point.stackTotal;
+       }
+   },
+   plotOptions: {
+       column: {
+           stacking: 'normal',
+           point: {
+               events: {
+                   click: function () {
+                       console.log('this.x,y=' + this.x + "," + this.y + " category=" + this.category);
 
-//                    }
-//                }
-//            },
-//            dataLabels: {
-//                enabled: true,
-//                color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-//            }
-//        }
-//    },
-//    
-//    plotOptions: {
-//        series: {
-//            marker: {
-//                enabled: false,
-//                id: "my scrollbar"
-//            }
-//        },
-//        column: {
-//            pointPlacement: "between",
-//            pointStart: 0,
-//            pointPadding: -(1.0/3),
-//            borderWidth: 0
-//        }
-//    },
-//    
-//    series: [{
-//        data: sunData
-//        }]
-//    
-//    
-// });
-// sunChart.series[0].setData(sunData);
+                   }
+               }
+           },
+           dataLabels: {
+               enabled: true,
+               color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+           }
+       }
+   },
+   
+   plotOptions: {
+       series: {
+           marker: {
+               enabled: false,
+               id: "my scrollbar"
+           }
+       },
+       column: {
+           pointPlacement: "between",
+           pointStart: 0,
+           pointPadding: -(1.0/3),
+           borderWidth: 0
+       }
+   },
+   
+   series: [{
+       data: sunData
+       }]
+   
+   
+});
+sunChart.series[0].setData(sunData);
   
 
 
--- PartnerLabs/MIT/BEElab/www/scripts/chartAll.js
+++ PartnerLabs/MIT/BEElab/www/scripts/chartAll.js
@@ -13,6 +13,71 @@
       return chart1.series[seriesIndex].setData(newData, true);
     };
 
+    var copyArray = function (array, x) {
+      newArray = new Array();
+      for (var i = 0; i < x; i++){
+        newArray = newArray.concat(array);
+      }
+      return newArray;
+    }
+
+    var sunHours = 10;
+    var sld = null;
+    var sunChart = null;
+
+    sunData = new Array();
+     for (var i = 0; i < sunHours; i++) {
+        sunData.push(100);
+     }
+     for (var i = 0; i < 24-sunHours; i++) {
+        sunData.push(0);
+     }
+     sunData = copyArray(sunData, this.lab.length);
+
+    var shiftSunGraph = function (sliderValue, sliderObj) {
+        newData = new Array();
+        for (var i = 0; i < sliderValue; i++) {
+            newData.push(0);
+        }
+        for (var i = 0; i < sunHours; i++) {
+            newData.push(100);
+        }
+        for (var i = 0; i < 24-sunHours-sliderValue; i++) {
+            newData.push(0);
+        }
+        sunData = newData;
+        //this updates the chart with the new start position of the sun
+        //chart.series[0].update({data: sunData}, true);
+        for (var i=0; i < chart1.series.length; i++){
+          if (chart1.series[i].name === 'sunChart') {
+            chart1.series[i].setData(copyArray(newData, this.lab.lenght/24));
+          }
+        }
+        sunChart.series[0].setData(newData);
+        return null;
+     }
+     var generateInvisibleSeries = function () {
+      var invisSeries = new Array();
+      for (var i=0; i < this.lab.lenght) {
+        invisSeries.push(400);
+      }
+      return invisSeries;
+     }
+
+    $(function() {
+        $("#sun_hours_select").change(function () {
+            sunHours = parseInt($("#sun_hours_select").val());
+            shiftSunGraph(0, null);
+            sld.setMax(24-sunHours);
+        })
+    });
+
+     //initialize the slider.  may not want 1500px size, need to test and see.
+     sld = new dhtmlxSlider("sunslider-container", 1200, "arrow", false, 0, 24-sunHours, 0, 1);
+     sld.setImagePath("scripts/dhtmlxSlider/codebase/imgs/");
+     sld.attachEvent("onChange", shiftSunGraph);
+     sld.init();
+
     LoadProfile.prototype.addToCurrentLoad = function(xPosition) {
     	console.log("LoadProfile.prototype.addToCurrentLoad called");
       var currentLoad;
@@ -87,7 +152,7 @@
         yAxis: [{
           max: 500,
           title: {
-            text: 'Total Wattz'
+            text: 'Total Watts'
           },
           tickInterval: 100,
           showLastLabel: true,
@@ -169,6 +234,51 @@
           });
           
         }
+        // invisible series to raise sun series to top
+        _results.push({
+          name: 'invisibleSeries',
+          data: generateInvisibleSeries(),
+          cursor: null,
+          visible: false,
+          point: {
+            events: {
+              click: function(event) {
+                return null;
+              }
+            }
+          },
+          column: {
+            borderWidth: 2,
+            pointPadding: 0,
+            groupPadding: 0,
+            stacking: 'sunStack',
+            dataLabels: {
+              enabled: false
+            }
+          }
+        })
+        // sunchart series
+        _results.push({
+          name: 'sunchart',
+          data: copyArray(sunData, this.lab.lenght/24),
+          cursor: null,
+          point: {
+            events: {
+              click: function(event) {
+                return null;
+              }
+            }
+          },
+          column: {
+            borderWidth: 2,
+            pointPadding: 0,
+            groupPadding: 0,
+            stacking: 'sunStack',
+            dataLabels: {
+              enabled: false
+            }
+          }
+        })
         console.log("In for loop  after _results.push");
         return _results;
       }).call(this);
